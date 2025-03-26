3eef753d8ff1aca17a491604489c7cdba91c28e8
beom710 <beom710@naver.com>
2025-03-26 15:35:34 +0900

    feat : Ultra , short 에 대한 격자정보 저장 및 호출 스케쥴러

--- src/main/kotlin/com/project/byeoldori/api/WeatherData.kt
+++ src/main/kotlin/com/project/byeoldori/api/WeatherData.kt
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Mono
+import org.slf4j.LoggerFactory
+
 
 @Component
 class WeatherData(
@@ -12,6 +14,7 @@ class WeatherData(
     private val weatherApiClient: WebClient,
     private val weatherApiProperties: WeatherApiProperties  // API Key 주입
 ) {
+    private val logger = LoggerFactory.getLogger(this::class.java)
     // AuthKey ".env"에서 가져옴
     private val weatherApiKey: String by lazy { weatherApiProperties.key }
     // 실황 조회
@@ -28,7 +31,20 @@ class WeatherData(
             .bodyToMono(String::class.java)
     }
 
-    // 초단기 격자 예보조회
+//    // 초단기 격자 예보조회
+//    fun fetchUltraShortForecast(tmfc: String, tmef: String, vars: String): Mono<String> {
+//        return weatherApiClient.get()
+//            .uri { builder ->
+//                builder.path("/cgi-bin/url/nph-dfs_vsrt_grd")
+//                    .queryParam("tmfc", tmfc)
+//                    .queryParam("tmef", tmef)
+//                    .queryParam("vars", vars)
+//                    .queryParam("authKey", weatherApiKey)
+//                    .build()
+//            }
+//            .retrieve()
+//            .bodyToMono(String::class.java)
+//    }
     fun fetchUltraShortForecast(tmfc: String, tmef: String, vars: String): Mono<String> {
         return weatherApiClient.get()
             .uri { builder ->
@@ -41,7 +57,20 @@ class WeatherData(
             }
             .retrieve()
             .bodyToMono(String::class.java)
+            // 구독 시작 시점에 로그
+            .doOnSubscribe {
+                logger.info("초단기예보 API 호출 시작 - tmfc=$tmfc, tmef=$tmef, vars=$vars")
+            }
+            // 성공 시점에 로그
+            .doOnSuccess {
+                logger.info("초단기예보 API 호출 성공 - tmfc=$tmfc, tmef=$tmef, vars=$vars")
+            }
+            // 에러 발생 시점에 로그
+            .doOnError { error ->
+                logger.error("초단기예보 API 호출 실패 - tmfc=$tmfc, tmef=$tmef, vars=$vars", error)
+            }
     }
+
 
     // 단기 격자 예보조회
     fun fetchShortForecast(tmfc: String, tmef: String, vars: String): Mono<String> {
@@ -56,6 +85,18 @@ class WeatherData(
             }
             .retrieve()
             .bodyToMono(String::class.java)
+            // 구독 시작 시점에 로그
+            .doOnSubscribe {
+                logger.info("단기예보 API 호출 시작 - tmfc=$tmfc, tmef=$tmef, vars=$vars")
+            }
+            // 성공 시점에 로그
+            .doOnSuccess {
+                logger.info("단기예보 API 호출 성공 - tmfc=$tmfc, tmef=$tmef, vars=$vars")
+            }
+            // 에러 발생 시점에 로그
+            .doOnError { error ->
+                logger.error("단기예보 API 호출 실패 - tmfc=$tmfc, tmef=$tmef, vars=$vars", error)
+            }
     }
 
     // 중기 육상 예보 조회
