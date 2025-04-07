40c85e226eb9dd14f9f10b41643238cd91dc5ca2
Taeyoung <xodud3084@naver.com>
2025-04-07 22:45:44 +0900

    시 지역코드와 (x,y)좌표 매핑 -> RegionMappingLoader.kt에서 json을 불러옴 좌표 매핑에 따른 Service,RegionMapper 수정

--- src/main/kotlin/com/project/byeoldori/service/ForeCastService.kt
+++ src/main/kotlin/com/project/byeoldori/service/ForeCastService.kt
@@ -17,9 +17,9 @@ class ForeCastService(
 
         // 1) 위경도 -> 격자x 좌표 변환
         val (x, y) = latLonToGrid(latitude, longitude)
-        val siRegId = getNearestSiRegIdByLocation(latitude, longitude) // 위경도로 매핑, 임시 방식 적용
-        // val siRegId = RegionMapper.getSiByGrid(x, y) ?: "UNKNOWN" 이건 격자 좌표로 매핑, 좌표와 시지역코드 매핑 시켜줘야함
-        val doRegId = RegionMapper.getDoBySi(siRegId) ?: "UNKNOWN"
+
+        // 1-2) 위경도 -> 좌표로 변환 후 시 지역 코드로 매핑
+        val siRegId = RegionMapper.getSiByLatLon(latitude, longitude) ?: "UNKNOWN"
 
         // 2) 초단기, 단기 예보
         val ultraForecast: List<UltraForecastResponseDTO> = ultraGridForecastService.getAllUltraTMEFDataForCell(x, y)
@@ -34,14 +34,5 @@ class ForeCastService(
             shortForecastResponse = shortForecast,
             midCombinedForecastDTO = midCombinedForecast
         )
-    }
-
-    // 임시 매핑 함수로 위도,경도 -> 시 코드 변환
-    private fun getNearestSiRegIdByLocation(lat: Double, lon: Double): String {
-        return when {
-            lat in 37.4..37.6 && lon in 126.9..127.1 -> "11B10101" // 서울 종로구
-            lat in 36.6..36.7 && lon in 127.3..127.5 -> "11C10301" // 대전 유성구
-            else -> "11B10101" // 기본값: 서울
-        }
     }
 }
