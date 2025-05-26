0e57ec8cf9ee0460510ee3490b1d898a6bab489e
Taeyoung <xodud3084@naver.com>
2025-05-26 21:45:42 +0900

    사용자 계정관리 기능 구현 - 사용자 요청 DTO: 회원가입, 로그인, 회원정보 수정, 이메일 찾기, 비밀번호 재설정 및 토큰 재발급 요청 객체 구현
    1. SignupRequestDto: 회원가입 시 이메일, 비밀번호, 약관 동의 정보 수신용
    
    2. LoginRequestDto: 로그인 요청 시 이메일 및 비밀번호 입력값 수신
    
    3. FindEmailRequestDto: 이메일 찾기용 사용자 정보 입력
    
    4. PasswordResetRequestDto: 비밀번호 재설정 요청 할 경우 이메일 기반 토큰 요청
    
    5. PasswordResetConfirmDto: 토큰과 새 비밀번호를 포함한 비밀번호 재설정 완료 요청
    
    6. TokenReissueRequestDto: 액세스/리프레시 토큰 재발급 요청
    
    7. UserUpdateRequestDto: 닉네임, 생년월일 사용자 정보 수정

---
+++ src/main/kotlin/com/project/byeoldori/user/dto/LoginRequestDto.kt
@@ -0,0 +1,13 @@
+package com.project.byeoldori.user.dto
+
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
+
+data class LoginRequestDto(
+    @field:Email(message = "유효한 이메일 형식이어야 합니다.")
+    @field:NotBlank(message = "이메일은 필수 입력 항목입니다.")
+    val email: String,
+
+    @field:NotBlank(message = "비밀번호는 필수 항목입니다.")
+    val password: String
+)
