8f237c18600359c8aa906b310f082d2207cf3ce2
Taeyoung <xodud3084@naver.com>
2025-09-24 23:07:16 +0900

    Forecast 기능 보완 1. 날씨 데이터 받아올 때 한국 이외의 좌표는 에러 표출, 한국 위경도 범위도 출력
    2. 단기 데이터 안정화

--- src/main/kotlin/com/project/byeoldori/forecast/controller/WeatherController.kt
+++ src/main/kotlin/com/project/byeoldori/forecast/controller/WeatherController.kt
@@ -2,16 +2,13 @@ package com.project.byeoldori.forecast.controller
 
 import com.project.byeoldori.forecast.dto.*
 import com.project.byeoldori.forecast.service.*
-import io.swagger.v3.oas.annotations.Operation
 import org.slf4j.LoggerFactory
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/weather")
 class WeatherController(
-    private val foreCastService: ForeCastService,
-    private val midCombinedForecastService: MidCombinedForecastService,
+    private val foreCastService: ForeCastService
 ) {
     private val logger = LoggerFactory.getLogger(this::class.java)
 
@@ -22,19 +19,5 @@ class WeatherController(
     ): ForecastResponseDTO {
         logger.info("ForecastData 호출 lat $lat, long $long")
         return foreCastService.getForecastDataByLocation(lat, long)
-    }
-
-    @Operation(summary = "중기 육상 + 기온 예보 조회", description = "기상청 중기 육상 + 예보 데이터를 호출 후 DB에 저장합니다.")
-    @PostMapping("/mid-combined")
-    fun fetchAndSaveMidCombinedForecastFromApi(): ResponseEntity<String> {
-        val result = midCombinedForecastService.fetchAndSaveFromApi()
-        return ResponseEntity.ok(result)
-    }
-
-    @Operation(summary = "저장된 중기 예보 전체 조회", description = "DB에 저장된 병합된 중기 육상 및 기온 데이터를 모두 조회합니다.")
-    @GetMapping("/mid-combined/all")
-    fun getAllSavedForecasts(): ResponseEntity<List<MidCombinedForecastDTO>> {
-        val dtoList = midCombinedForecastService.findAll()
-        return ResponseEntity.ok(dtoList)
     }
 }
