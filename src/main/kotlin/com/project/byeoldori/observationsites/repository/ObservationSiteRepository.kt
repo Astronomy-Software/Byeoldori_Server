c5be3e4df8765d3c3e4ceecb96fb79e9f9ded377
Taeyoung <xodud3084@naver.com>
2025-09-24 23:35:05 +0900

    관측지 수정 및 삭제에서 name이 아닌 id 사용

--- src/main/kotlin/com/project/byeoldori/observationsites/repository/ObservationSiteRepository.kt
+++ src/main/kotlin/com/project/byeoldori/observationsites/repository/ObservationSiteRepository.kt
@@ -1,15 +1,11 @@
 package com.project.byeoldori.observationsites.repository
 
 import com.project.byeoldori.observationsites.entity.ObservationSite
-import jakarta.transaction.Transactional
 import org.springframework.data.jpa.repository.JpaRepository
 
-@Transactional
 interface ObservationSiteRepository : JpaRepository<ObservationSite, Long> {
     fun findByName(name: String): ObservationSite?
     fun findByNameContaining(keyword: String): List<ObservationSite>
-    fun deleteByName(name: String)
     fun existsByName(name: String): Boolean
     fun existsByNameAndLatitudeAndLongitude(name: String, latitude: Double, longitude: Double): Boolean
-
 }
--- src/main/kotlin/com/project/byeoldori/observationsites/repository/UserSavedSiteRepository.kt
+++ src/main/kotlin/com/project/byeoldori/observationsites/repository/UserSavedSiteRepository.kt
@@ -7,5 +7,5 @@ interface UserSavedSiteRepository : JpaRepository<UserSavedSite, Long> {
     fun existsByUserIdAndSiteId(userId: String, siteId: Long): Boolean
     fun findByUserId(userId: String): List<UserSavedSite>
     fun deleteByUserIdAndSiteId(userId: String, siteId: Long)
-
+    fun deleteBySiteId(siteId: Long)
 }
