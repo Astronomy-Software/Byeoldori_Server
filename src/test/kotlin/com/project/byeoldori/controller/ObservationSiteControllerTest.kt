4a84d31e27a0ccede9fdbabb266433ebf84e0698
Taeyoung <xodud3084@naver.com>
2025-10-04 18:04:03 +0900

    - 관측 적합도 기능 변경에 따른 테스트 코드 변경

--- src/test/kotlin/com/project/byeoldori/controller/ObservationSiteControllerTest.kt
+++ src/test/kotlin/com/project/byeoldori/controller/ObservationSiteControllerTest.kt
@@ -3,9 +3,7 @@ package com.project.byeoldori.controller
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.project.byeoldori.observationsites.controller.ObservationSiteController
 import com.project.byeoldori.observationsites.dto.ObservationSiteDto
-import com.project.byeoldori.observationsites.dto.RecommendationRequestDto
 import com.project.byeoldori.observationsites.entity.ObservationSite
-import com.project.byeoldori.observationsites.service.ObservationSiteRecommendationService
 import com.project.byeoldori.observationsites.service.ObservationSiteService
 import org.hamcrest.Matchers.*
 import org.junit.jupiter.api.BeforeEach
@@ -19,10 +17,8 @@ import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
-import java.time.LocalDateTime
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.databind.SerializationFeature
-import com.project.byeoldori.observationsites.dto.ObservationSiteResponseDto
 
 private val objectMapper: ObjectMapper = ObjectMapper().apply {
     registerModule(JavaTimeModule())
@@ -35,13 +31,12 @@ class ObservationSiteControllerTest {
     @Mock
     private lateinit var siteService: ObservationSiteService
     @Mock
-    private lateinit var siteRecommendationService: ObservationSiteRecommendationService
     private lateinit var controller: ObservationSiteController
     private lateinit var mockMvc: MockMvc
 
     @BeforeEach
     fun setup() {
-        controller = ObservationSiteController(siteService, siteRecommendationService)
+        controller = ObservationSiteController(siteService)
         mockMvc = MockMvcBuilders.standaloneSetup(controller).build()
     }
 
@@ -92,7 +87,7 @@ class ObservationSiteControllerTest {
         val dto = ObservationSiteDto("한라산", 33.0, 126.0)
         val updated = ObservationSite(2, "한라산", 33.0, 126.0)
 
-        `when`(siteService.updateSiteByName("지리산", dto)).thenReturn(updated)
+        `when`(siteService.updateSiteById(2, dto)).thenReturn(updated)
 
         mockMvc.perform(
             put("/observationsites/지리산")
@@ -105,43 +100,9 @@ class ObservationSiteControllerTest {
 
     @Test
     fun `관측지 삭제`() {
-        doNothing().`when`(siteService).deleteSiteByName("지리산")
+        doNothing().`when`(siteService).deleteSiteById(2)
 
         mockMvc.perform(delete("/observationsites/지리산"))
             .andExpect(status().isNoContent)
-    }
-
-    @Test
-    fun `관측지 추천`() {
-        val sampleRecommendation = listOf(
-            ObservationSiteResponseDto("별마로 천문대", 37.197877, 128.486595, 0.85),
-            ObservationSiteResponseDto("충북대학교", 37.5, 127.5, 0.65)
-        )
-
-        val request = RecommendationRequestDto(
-            userLat = 37.5665,
-            userLon = 126.9780,
-            observationTime = LocalDateTime.of(2025, 4, 29, 21, 0)
-        )
-
-        `when`(
-            siteRecommendationService.recommendSites(
-                userLat = request.userLat,
-                userLon = request.userLon,
-                observationTime = request.observationTime
-            )
-        ).thenReturn(sampleRecommendation)
-
-        mockMvc.perform(
-            post("/observationsites/recommend")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(request))
-        )
-            .andExpect(status().isOk)
-            .andExpect(jsonPath("$.size()", `is`(2)))
-            .andExpect(jsonPath("$[0].name", `is`("별마로 천문대")))
-            .andExpect(jsonPath("$[0].score", `is`(0.85)))
-            .andExpect(jsonPath("$[1].name", `is`("충북대학교")))
-            .andExpect(jsonPath("$[1].score", `is`(0.65)))
     }
 }
