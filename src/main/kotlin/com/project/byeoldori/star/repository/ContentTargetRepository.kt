06cb4ddd715d6f822e1f1ddaed9347b57b26734d
Taeyoung <xodud3084@naver.com>
2025-11-06 00:41:12 +0900

    관측 대상으로 조회 - 게시글의 적힌 관측 대상으로 해당 천체의 게시글 조회

--- src/main/kotlin/com/project/byeoldori/star/repository/ContentTargetRepository.kt
+++ src/main/kotlin/com/project/byeoldori/star/repository/ContentTargetRepository.kt
@@ -7,21 +7,26 @@ import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 interface ContentTargetRepository : JpaRepository<ContentTarget, Long> {
+
     @Modifying
     @Query("delete from ContentTarget ct where ct.contentType = :type and ct.contentId = :id")
-    fun deleteAllByContent(type: ContentType, id: Long): Int
+    fun deleteAllByContent(
+        @Param("type") type: ContentType,
+        @Param("id") id: Long
+    ): Int
 
     @Query("""select ct.contentId from ContentTarget ct where ct.contentType = :type and ct.starObjectName = :name""")
     fun findContentIdsByStar(
-        type: ContentType,
-        name: String,
+        @Param("type") type: ContentType,
+        @Param("name") name: String,
         pageable: Pageable
     ): Page<Long>
 
-    fun findAllByContentTypeAndContentId(type: ContentType, contentId: Long): List<ContentTarget>
-
-    @Query("""select ct from ContentTarget ct where ct.contentType = :type and ct.contentId in :ids""")
-    fun findAllByTypeAndContentIds(type: ContentType, ids: Collection<Long>): List<ContentTarget>
+    fun findAllByContentTypeAndContentId(
+        type: ContentType,
+        contentId: Long
+    ): List<ContentTarget>
 }
